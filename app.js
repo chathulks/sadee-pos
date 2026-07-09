5a0d064b7173704128e5cb6ad18eac0f68b29f9d
chathulks <sadeeshachathumal5@gmail.com>
2026-07-10 00:16:45 +0530

    feat: add onclick handler to Add button for customer addition

---
+++ app.js
@@ -0,0 +1,12 @@
+function customerAddOnAction(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let mobile = document.getElementById("mobile").value;
+    let email = document.getElementById("email").value;
+    let city = document.getElementById("city").value;
+    let add_line_1 = document.getElementById("add-line-1").value;
+    let add_line_2 = document.getElementById("add-line-2").value;
+    let gender = document.getElementById("gender").value;
+    let fname = document.getElementById("fname").value;
+    let fname = document.getElementById("fname").value;
+}
