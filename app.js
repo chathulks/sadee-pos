6ea06fa4b68e93e58b38bd6a8825817e1fc1ef0c
chathulks <sadeeshachathumal5@gmail.com>
2026-07-10 10:40:37 +0530

    feat: implement AddCustomer function and create HTML files for brand, category, company, customer, employee, product, sales, and stock

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function customerAddOnAction(){
+function AddCustomer() {
     let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let mobile = document.getElementById("mobile").value;
@@ -7,6 +7,21 @@ function customerAddOnAction(){
     let add_line_1 = document.getElementById("add-line-1").value;
     let add_line_2 = document.getElementById("add-line-2").value;
     let gender = document.getElementById("gender").value;
-    let fname = document.getElementById("fname").value;
-    let fname = document.getElementById("fname").value;
+    let status = document.getElementById("status").value;
+    let barcode = document.getElementById("barcode").value;
+
+    let Custmoer = {
+        fname: fname,
+        lname: lname,
+        mobile: mobile,
+        email: email,
+        city: city,
+        add_line_1: add_line_1,
+        add_line_2: add_line_2,
+        gender: gender,
+        status: status,
+        barcode: barcode
+    }
+
+    console.log(Custmoer);
 }
